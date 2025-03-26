5046fe9bed2f76157cffe2f9717c3b3d5d80bfc2
agberk <aaron@aaronberk.com>
2025-03-26 05:45:45 +0000

    Add keypoints to TRT_NMS.symbolic

--- models/experimental.py
+++ models/experimental.py
@@ -116,29 +116,36 @@ class TRT_NMS(torch.autograd.Function):
         return num_det, det_boxes, det_scores, det_classes, det_keypoints
 
     @staticmethod
-    def symbolic(g,
-                 boxes,
-                 scores,
-                 background_class=-1,
-                 box_coding=1,
-                 iou_threshold=0.45,
-                 max_output_boxes=100,
-                 plugin_version="kpt",
-                 score_activation=0,
-                 score_threshold=0.25):
-        out = g.op("TRT::EfficientNMS_TRT",
-                   boxes,
-                   scores,
-                   background_class_i=background_class,
-                   box_coding_i=box_coding,
-                   iou_threshold_f=iou_threshold,
-                   max_output_boxes_i=max_output_boxes,
-                   plugin_version_s=plugin_version,
-                   score_activation_i=score_activation,
-                   score_threshold_f=score_threshold,
-                   outputs=4)
-        nums, boxes, scores, classes = out
-        return nums, boxes, scores, classes
+    def symbolic(
+            g,
+            boxes,
+            scores,
+            keypoints,
+            background_class=-1,
+            box_coding=1,
+            iou_threshold=0.45,
+            max_output_boxes=100,
+            plugin_version="kpt",
+            score_activation=0,
+            score_threshold=0.25
+    ):
+        out = g.op(
+            "TRT::EfficientNMS_TRT",
+            boxes,
+            scores,
+            keypoints,
+            background_class_i=background_class,
+            box_coding_i=box_coding,
+            iou_threshold_f=iou_threshold,
+            max_output_boxes_i=max_output_boxes,
+            plugin_version_s=plugin_version,
+            score_activation_i=score_activation,
+            score_threshold_f=score_threshold,
+            outputs=5
+        )
+        nums, boxes, scores, classes, keypoints = out
+
+        return nums, boxes, scores, classes, keypoints
 
 
 class ONNX_ORT(nn.Module):
